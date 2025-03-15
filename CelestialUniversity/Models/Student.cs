5a64d183c46fa7db0d3673b384bd60220d9f4cfe
sjjunker <sandi.junker@outlook.com>
2025-03-14 21:15:31 -0500

    Edit Student model display names and types.

--- CelestialUniversity/Models/Student.cs
+++ CelestialUniversity/Models/Student.cs
@@ -1,10 +1,19 @@
-﻿namespace CelestialUniversity.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CelestialUniversity.Models
 {
     public class Student
     {
         public int ID { get; set; }
+
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Display(Name = "First Name")]
         public string FirstMidName { get; set; }
+
+        [Display(Name = "Enrollment Date")]
+        [DataType(DataType.Date)]
         public DateTime EnrollmentDate { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
