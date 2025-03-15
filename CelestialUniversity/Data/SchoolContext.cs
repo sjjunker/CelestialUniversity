b663433a4e56ac185397c5e523dcb68f3a4ef825
sjjunker <sandi.junker@outlook.com>
2025-03-14 19:50:42 -0500

    Add project files.

---
+++ CelestialUniversity/Data/SchoolContext.cs
@@ -0,0 +1,23 @@
+﻿using CelestialUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CelestialUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; } = null!;
+        public DbSet<Enrollment> Enrollments { get; set; } = null!;
+        public DbSet<Student> Students { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
