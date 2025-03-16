54c8acfb3fa6817972ebc4cb6650184e8eec78e2
sjjunker <sandi.junker@outlook.com>
2025-03-15 19:18:20 -0500

    Add sort/filter/page/group, About page, and some styling

--- CelestialUniversity/Controllers/HomeController.cs
+++ CelestialUniversity/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CelestialUniversity.Data;
+using CelestialUniversity.Models.SchoolViewModels;
+using Microsoft.Extensions.Logging;
 using CelestialUniversity.Models;
 
 namespace CelestialUniversity.Controllers;
@@ -7,15 +11,30 @@ namespace CelestialUniversity.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly SchoolContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, SchoolContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
     {
         return View();
+    }
+
+    public async Task<ActionResult> About()
+    {
+        IQueryable<EnrollmentDateGroup> data =
+            from student in _context.Students
+            group student by student.EnrollmentDate into dateGroup
+            select new EnrollmentDateGroup()
+            {
+                EnrollmentDate = dateGroup.Key,
+                StudentCount = dateGroup.Count()
+            };
+        return View(await data.AsNoTracking().ToListAsync());
     }
 
     public IActionResult Privacy()
