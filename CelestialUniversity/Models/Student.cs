1d3c23b08546c856dcd75328397ef58b56372a12
sjjunker <sandi.junker@outlook.com>
2025-03-17 18:29:14 -0500

    Inheritance

--- CelestialUniversity/Models/Student.cs
+++ CelestialUniversity/Models/Student.cs
@@ -3,35 +3,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CelestialUniversity.Models
 {
-    public class Student
+    public class Student : Person
     {
-        public int ID { get; set; }
-
-        [Required]
-        [Display(Name = "Last Name")]
-        [StringLength(50)]
-        public string LastName { get; set; }
-
-        [Required]
-        [Display(Name = "First Name")]
-        [StringLength(50)]
-        [Column("FirstName")]
-        public string FirstMidName { get; set; }
-
         [Required]
         [DataType(DataType.Date)]
         [Display(Name = "Enrollment Date")]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
-
-        [Display(Name = "Full Name")]
-        public string FullName
-        {
-            get
-            {
-                return LastName + ", " + FirstMidName;
-            }
-        }
 
         public ICollection<Enrollment> Enrollments { get; set; } = [];
     }
